5b286d0e98e5494673446e7d497ed767476e1065
ashishpatel-dd1 <ashishbond00726@gmail.com>
2025-01-29 16:03:12 +0530

    added animations on application launch

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              SizedBox(
-                height: 72.0,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
+      home: _AnimatedProfileScreen(),
+    );
+  }
+}
+
+class _AnimatedProfileScreen extends StatefulWidget {
+  @override
+  _AnimatedProfileScreenState createState() => _AnimatedProfileScreenState();
+}
+
+class _AnimatedProfileScreenState extends State<_AnimatedProfileScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1200),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    _slideAnimation = Tween<Offset>(begin: Offset(0, 0.3), end: Offset.zero)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SizedBox(height: 72.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ScaleTransition(
+                  scale: _scaleAnimation,
+                  child: CircleAvatar(
                     radius: 64.0,
                     backgroundImage: AssetImage('images/ashishpatel.jpg'),
                   ),
-                  Text(
+                ),
+                SizedBox(height: 12.0),
+                FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: Text(
                     'Ashish Patel',
                     style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 32.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2.5),
+                      fontFamily: 'Pacifico',
+                      fontSize: 32.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5,
+                    ),
                   ),
-                  Text(
+                ),
+                FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: Text(
                     'FLUTTER, ML, LINUX',
                     style: TextStyle(
                       fontFamily: 'Roboto',
@@ -45,107 +101,73 @@ class MyApp extends StatelessWidget {
                       letterSpacing: 1.5,
                     ),
                   ),
-                  SizedBox(
-                    height: 16.0,
-                    width: 144.0,
-                    child: Divider(
-                      color: Colors.tealAccent,
-                    ),
-                  ),
-                  Card(
+                ),
+                SizedBox(
+                  height: 16.0,
+                  width: 144.0,
+                  child: Divider(color: Colors.tealAccent),
+                ),
+                SlideTransition(
+                  position: _slideAnimation,
+                  child: Card(
                     margin:
                         EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-                    child: Padding(
-                      padding: const EdgeInsets.all(0.0),
-                      child: ListTile(
-                        leading: Icon(
-                          Icons.phone,
-                          color: Colors.teal[700],
-                        ),
-                        title: Text(
-                          '+91 8999205770',
-                          style: TextStyle(
-                            color: Colors.teal[900],
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
-                          ),
+                    child: ListTile(
+                      leading: Icon(Icons.phone, color: Colors.teal[700]),
+                      title: Text(
+                        '+91 8999205770',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
                         ),
                       ),
                     ),
                   ),
-                  Card(
+                ),
+                SlideTransition(
+                  position: _slideAnimation,
+                  child: Card(
                     margin:
                         EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-                    child: Padding(
-                      padding: const EdgeInsets.all(0.0),
-                      child: ListTile(
-                        leading: Icon(
-                          Icons.email,
-                          color: Colors.teal[700],
-                        ),
-                        title: Text(
-                          'ashishpatel9119@hotmail.com',
-                          style: TextStyle(
-                            color: Colors.teal[900],
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
-                          ),
+                    child: ListTile(
+                      leading: Icon(Icons.email, color: Colors.teal[700]),
+                      title: Text(
+                        'ashishpatel9119@hotmail.com',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 16.0,
-                    width: 144.0,
-                    child: Divider(
-                      color: Colors.tealAccent,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 144.0,
-              ),
-              Row(
+                ),
+                SizedBox(
+                  height: 16.0,
+                  width: 144.0,
+                  child: Divider(color: Colors.tealAccent),
+                ),
+              ],
+            ),
+            SizedBox(height: 144.0),
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(
-                    Icons.copyright,
-                    size: 16.0,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 4.0,
-                  ),
+                  Icon(Icons.copyright, size: 16.0, color: Colors.white),
+                  SizedBox(width: 4.0),
                   Text(
                     'SOEUQINU.ORG (2025)',
                     style: TextStyle(color: Colors.white, fontSize: 14.0),
                   ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-// Row(
-// children: <Widget>[
-// Icon(
-// Icons.phone,
-// color: Colors.teal[700],
-// ),
-// SizedBox(
-// width: 16.0,
-// ),
-// Text(
-// '+91 8999205770',
-// style: TextStyle(
-// color: Colors.teal[900],
-// fontWeight: FontWeight.bold,
-// fontSize: 16.0,
-// ),)
-// ],
-// )
